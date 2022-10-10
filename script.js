dcab67a4b78916070b80dd4235280e9239811386
Abena Agyapomaa <amoakobaidoogertrude@gmail.com>
2022-10-10 13:03:23 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let modal=document.getElementById('modal')
+let add1=document.getElementById('add1')
+let submit=document.getElementById('submit')
+let but=document.getElementById('but')
+let book=document.getElementById('book')
+
+
+
+
+
+
+
+
+
+but.onclick=function(){
+    modal.style.display='flex';
+}
+add1.onclick=function(){
+    modal.style.display='none';
+}
+
